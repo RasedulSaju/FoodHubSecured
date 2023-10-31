bd6f8817563c405a773bcee446a7f0fa6488f8ae
Rasedul Islam <rasedulsaju@gmail.com>
2023-10-31 11:53:31 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    $dbserver="localhost"; // Database Server or Host
+    $dbuser="root"; // Database User
+    $dbpass=""; // Database User's Password
+    $db="lab_foodhub"; // Database Name
+    $dbport="3306"; // Database Port
+?>
